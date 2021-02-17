6629da4f6d78932f7a59171d7264650de32d9330
Robertrama <ramascanurobert@gmail.com>
2021-02-17 11:33:37 +0100

    upgrade the step

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="ramascanurobert/project"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+    docker image tag project $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
