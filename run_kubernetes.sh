83a694dad0c03d5b28cbbca922cb31c9f62b21d3
Robertrama <ramascanurobert@gmail.com>
2021-02-22 10:14:09 +0100

    add the step 2

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ramascanurobert/project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+#kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+#kubectl port-forward udacity 8000:80
 
